15e85573efa874af6bf106a00135a438f397a569
Eddu <azabacheeduardo85@gmail.com>
2025-09-03 11:01:32 -0500

    tarea

---
+++ main.py
@@ -0,0 +1,7 @@
+# funcion de alba
+# funcion de carlos
+# funcion de maryori
+# funcion de grezia
+
+# TAREA: IMPLEMENTAR UNA FUNCION EN UN FOLDER CON TU NOMBRE
+# IMPORTARLO EN ESTE ARCHIVO Y LLAMARLO
